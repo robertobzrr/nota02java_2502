7886961ef50bbd748f666532fee58c7697cf46e6
robertobzrr <robertobezerrafh@outlook.com>
2025-03-11 14:29:05 -0300

    revisar final (problema retornar valor correto)

--- src/ControleProduto.java
+++ src/ControleProduto.java
@@ -1,4 +1,111 @@
-package PACKAGE_NAME;
-
 public class ControleProduto {
+
+        private int codigo, quantidadeEstoque, quantidadeCompra, opcaoPagamento;
+        private String nome, cor;
+        private double tamanho, peso, valor;
+
+
+             public void setCodigo (int codigo_digitado){
+                 this.codigo = codigo_digitado;
+             }
+             public int getCodigo(){
+                 return codigo;
+             }
+
+
+             public void setNome (String nome_digitado){
+                 this.nome = nome_digitado;
+             }
+             public String getNome(){
+                 return nome;
+             }
+
+
+             public void setTamanho (double tamanho_digitado){
+                this.tamanho = tamanho_digitado;
+             }
+             public double getTamanho(){
+                return tamanho;
+             }
+
+
+             public void setPeso (double peso_digitado){
+                this.peso = peso_digitado;
+             }
+             public double getPeso(){
+                return peso;
+             }
+
+
+             public void setCor (String cor_digitada){
+                this.cor = cor_digitada;
+             }
+             public String getCor(){
+                return cor;
+             }
+
+
+             public void setValor (double valor_digitado){
+                this.valor = valor_digitado;
+             }
+             public double getValor(){
+                return valor;
+             }
+
+
+             public void setQuantidadeEstoque (int quantidadeEstoque_digitado){
+                this.quantidadeEstoque = quantidadeEstoque_digitado;
+             }
+             public int getQuantidadeEstoque(){
+                return quantidadeEstoque;
+             }
+
+
+             public void setQuantidadeCompra (int quantidadeCompra_digitado){
+                 this.quantidadeCompra = quantidadeCompra_digitado;
+             }
+             public int getQuantidadeCompra(){
+                 return quantidadeCompra;
+             }
+
+
+             public void setOpcaoPagamento(int opcaoPagamento_digitado) {
+                 this.opcaoPagamento = opcaoPagamento_digitado;
+             }
+             public int getOpcaoPagamento() {
+                 return opcaoPagamento;
+             }
+
+
+
+        public double definirValor(double valor){
+               valor = valor * quantidadeCompra;
+               return valor;
+        }
+
+
+        public double atualizarEstoque(int quantidadeEstoque){
+               quantidadeEstoque = quantidadeEstoque - quantidadeCompra;
+               return quantidadeEstoque;
+        }
+
+
+        //if else com problema de mostrar o valor parcelado
+        public double aplicarDesconto(double OpcaoPagamento){
+
+                 if (OpcaoPagamento >= 1 && OpcaoPagamento <= 4){
+                     valor = quantidadeCompra - (quantidadeCompra * 0.05);
+                     return valor;
+                 }else if (OpcaoPagamento == 5){
+                     valor = valor / 3;
+                     return valor;
+                 }else {
+                     return valor;
+                 }
+
+        }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+                Scanner sc = new Scanner(System.in);
+                ControleProduto cp = new ControleProduto();
+
+
+        System.out.println("========== Exercício nota 2 ==========");
+        System.out.println("\n-> bem-vindo a aba de cadastro, vamo iniciar o cadastro do seu produto: ");
+
+        System.out.println(" - Digite o código do produto: ");
+        cp.setCodigo(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println(" - Digite o nome do produto: ");
+        cp.setNome(sc.nextLine());
+
+        System.out.println(" - Digite o tamanho do produto (em CM): ");
+        cp.setTamanho(sc.nextDouble());
+        sc.nextLine();
+
+        System.out.println(" - Digite o peso do produto (em KG): ");
+        cp.setPeso(sc.nextDouble());
+        sc.nextLine();
+
+        System.out.println(" - Digite a cor do produto: ");
+        cp.setCor(sc.nextLine());
+
+        System.out.println(" - Digite o valor do produto: ");
+        cp.setValor(sc.nextDouble());
+        sc.nextLine();
+
+        System.out.println(" - Digite a quantiade do produto em estoque: ");
+        cp.setQuantidadeEstoque(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println("código: "+cp.getCodigo()+ "\nnome: "+ cp.getNome()+ "\ntamanho: "+cp.getTamanho()+ "cm\npeso: "+cp.getPeso()+ "kg\ncor: "+cp.getCor()+ "\nvalor: "+ cp.getValor()+ "\nestoque: "+ cp.getQuantidadeEstoque());
+        System.out.println("\n\n-> Produto cadastrado com sucesso!!!\n\nVamos simular uma venda: ");
+        System.out.println("\nDigite quantas unidades deseja comprar: ");
+        cp.setQuantidadeCompra(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println("Valor total = "+ cp.definirValor(cp.getValor())+ "\nEscolha a forma de pagamento:\n[1] Pix\n[2] Dinheiro\n[3] Transferência\n[4] Débito\n[5] Crédito\n");
+        cp.setOpcaoPagamento(sc.nextInt());
+        sc.nextLine();
+
+        int i = 0;
+        switch(cp.getOpcaoPagamento()){
+            case 1, 2, 3, 4:
+                System.out.println("Desconto aplicado!!\nPreço final = "+cp.aplicarDesconto(cp.getValor()));
+                break;
+
+            case 5:
+                System.out.println("\"Valor dividido em 3x Parcelas de = "+cp.aplicarDesconto(cp.getValor()));
+                break;
+
+        }
+
+
+
+        System.out.println("Estoque atualizado = "+ cp.atualizarEstoque(cp.getQuantidadeEstoque()));
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
